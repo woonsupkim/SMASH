03dca7e6cdfcb2fdd0c586698e7042ef706770fc
Woon Kim <dwkim1212@gmail.com>
2023-09-04 21:35:55 -0400

    Update global.R

--- Ver 2.3/global.R
+++ Ver 2.3/global.R
@@ -30,11 +30,11 @@ pp1 <- matrix(
 
 # players Wimbledon - final 16
 plyrsW <- read_csv('data/smash_us.csv') %>% 
-  filter(wim_rd == 16)
+  filter(us_rd == 8)
 
 # players Wimbledon - final 16
 plyrsu <- read_csv('data/smash_us.csv') %>% 
-  filter(wim_rd == 16)
+  filter(us_rd == 8)
 
 # team members
 member <- c(
